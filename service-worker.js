4e2e3e583dd41be60e8daa4d1d1fd37c060508f7
Minami0824 <d9n4zkd2s6@privaterelay.appleid.com>
2026-09-18 00:22:21 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,3 @@
-const CACHE="mga-pwa-v1";const ASSETS=["./","./index.html","./manifest.webmanifest","./icon.svg"];
+const CACHE="mga-pwa-v3";const ASSETS=["./","./index.html","./manifest.webmanifest","./icon.svg"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
